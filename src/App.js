685e89b1acb0e732a8bf60be537040be319900d6
Folafunmi <folafunmimustapha@gmail.com>
2020-07-18 00:17:20 +0100

    Refactored JSX

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
+/*jshint esversion: 6*/
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    /* super() calls the constructor function on the component class
+    and that gives access to 'this.state'
+
+    'state' is a property that now exists on the class App and 
+    can be set to something
+    
+    Also provides access to setState*/
+    super();
+    
+    this.state = {
+      monsters: [
+        {name: "Frankenstein",
+         id: '1'},
+        {name: "Dracula",
+         id: '2'},
+        {name: "Zombie",
+         id: '3'}
+      ]
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => console.log(users));
+  }
+
+  render() {
+    return(
+      <div className="App">
+        { this.state.monsters.map(monster => (
+          <h1 key={monster.id}> {monster.name} </h1>
+        ))}
+      </div>
+    );
+  }
 }
+
 
 export default App;
