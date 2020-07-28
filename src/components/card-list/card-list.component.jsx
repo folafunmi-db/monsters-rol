53c881ce0e8f7c1a53954b9dbef0337d979960dc
Folafunmi <folafunmimustapha@gmail.com>
2020-07-28 02:00:33 +0100

    Completed card component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -5,12 +5,12 @@ import { Card } from '../card/card.component';
 import "./card-list.styles.css";
 
 /* Card list component is just responsible for rendering the cards it is
-not responsible for how they look. That is, this component just adds and 
-lists the cards*/
+not responsible for creating them or how they look. That is, this 
+component just adds and lists the cards*/
 export const CardList = props => (
     <div className='card-list'>
         <h1> { props.monsters.map(monster => (
-            <Card key={ monster.id } monster={ monster }/>
+            <Card key={ monster.id } monster={ monster } />
         ))} 
         </h1>
     </div>
