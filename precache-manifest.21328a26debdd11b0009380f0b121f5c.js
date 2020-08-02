43a4a4ef41e56ecb5064dc68388dadcff4b0d724
Folafunmi <folafunmimustapha@gmail.com>
2020-08-02 16:50:18 +0100

    Updates

---
+++ precache-manifest.21328a26debdd11b0009380f0b121f5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b2577b8ce1dbd1c282f73feaa518e3",
+    "url": "/monsters-rol/index.html"
+  },
+  {
+    "revision": "2e58fa237cbb09256ffe",
+    "url": "/monsters-rol/static/css/main.9cc1837c.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rol/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rol/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e58fa237cbb09256ffe",
+    "url": "/monsters-rol/static/js/main.37613440.chunk.js"
+  },
+  {
+    "revision": "3dbe3823855da6b3178c",
+    "url": "/monsters-rol/static/js/runtime-main.392135d2.js"
+  }
+]);
